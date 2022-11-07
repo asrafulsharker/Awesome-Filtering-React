bfc1d3db9e44a75ae8ad131042ac3408aee558e8
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-11-07 18:30:36 +0600

    add

--- src/movie.js
+++ src/movie.js
@@ -1,8 +1,11 @@
-function Movie(movie){
+function Movie({movie}){
     return(
         <div>
             <h2>Title</h2>
-            <img src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}/>
+            <img 
+            src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
+            alt=""
+            />
         </div>
     )
 }
